d5f95b0a1084b15962c3b184fc56b01d39801039
Nomozboy <nomoz.boy@mail.ru>
2023-01-05 00:51:11 +0500

    squad page qo'shildi

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class SquadPageView(TemplateView):
+    template_name = 'squad.html'
